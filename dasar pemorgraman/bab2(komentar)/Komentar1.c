a6b66a55396cf9b630753b786aa518a6f6554f46
Shinbazakura <zulfian478@upi.edu>
2026-04-08 00:07:51 +0700

    second commit

---
+++ dasar pemorgraman/bab2(komentar)/Komentar1.c
@@ -0,0 +1,6 @@
+#include <stdio.h> //ini adalah library untuk input output
+
+int main() {  //fungsi utama dalam program
+    printf("Hello, World!");  ///ini adalah fungsi untuk mencetak teks ke layar
+    return 0;//mengembalikan nilai 0 menandakan program selesai dengan sukses
+}     //akhir dari fungsi main
